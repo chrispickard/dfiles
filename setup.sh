4b76ba9a60e4bac5a9cd35e6ab4cea89dfa07237
Chris Pickard <chrispickard9@gmail.com>
2018-11-05 10:42:55 -0500

    updated setup script to install zgen and bash-it

--- setup.sh
+++ setup.sh
@@ -3,3 +3,9 @@
 for i in $(ls -d */); do
     stow -R $i
 done
+
+# install zgen
+git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+
+# install bash-it
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
