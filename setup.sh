7084babacc32f49ed045e799fa47d645a838c9d9
Chris Pickard <chrispickard9@gmail.com>
2018-11-05 11:29:09 -0500

    adding i3 config

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for i in $(ls -d */); do
+for i in $(ls -d */ | grep -v config); do
     stow -R $i
 done
 
@@ -9,3 +9,9 @@ git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 
 # install bash-it
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+
+mkdir -p $HOME/.config/i3
+ln -s $HOME/dfiles/config/i3_config $HOME/.config/i3/config
+
+mkdir -p $HOME/.config/alacritty
+ln -s $HOME/dfiles/config/alacritty.yml $HOME/.config/alacritty/alacritty.yml
