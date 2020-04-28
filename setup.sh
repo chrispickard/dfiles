bfbae1c85d7078a64b889818e8805164f468f636
Chris Pickard <chris.pickard@altamiracorp.com>
2020-04-28 09:49:46 -0400

    updated readme

--- setup.sh
+++ setup.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-command -v stow >/dev/null 2>&1 || { echo >&2 "I require gnu stow but it's not installed. Please install it with dnf/yum/apt install stow. Aborting."; exit 1; }
+# command -v stow >/dev/null 2>&1 || { echo >&2 "I require gnu stow but it's not installed. Please install it with dnf/yum/apt install stow. Aborting."; exit 1; }
 
-for i in $(ls -d */ | grep -v config | grep -v nix); do
-    stow -R $i
-done
+# for i in $(ls -d */ | grep -v config | grep -v nix); do
+#     stow -R $i
+# done
 
-# install zgen
-git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+# # install zgen
+# git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 
-# install asdf
-# git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.6.2
-
+# # install asdf
+# # git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.6.2
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+git clone -b develop https://github.com/syl20bnr/spacemacs ~/.emacs.d
 
 # use home-manager instead
 # mkdir -p $HOME/.config/i3
@@ -25,10 +26,18 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 mkdir -p $HOME/.config/tridactyl
 ln -s $HOME/dfiles/config/tridactylrc $HOME/.config/tridactyl/tridactylrc
 
-ln -s $HOME/dfiles/nix/* $HOME/.config/nixpkgs
+mkdir -p $HOME/.config/nixpkgs
+ln -s $HOME/dfiles/nix/config.nix $HOME/.config/nixpkgs/
+ln -s $HOME/dfiles/nix/home.nix $HOME/.config/nixpkgs/
+
+curl -L https://nixos.org/nix/install | sh
+
+nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+nix-shell '<home-manager>' -A install
 
 # instead of this, build github.com/chrispickard/btf and put that in /usr/local/bin
 # mkdir -p /usr/local/bin
 # sudo cp $HOME/dfiles/exe/btf /usr/local/bin
 
-fc-cache -fv
+# fc-cache -fv
