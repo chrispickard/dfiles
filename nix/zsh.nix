f5f6035906dbaf24e60ec97fa6b011a5f4982cfc
Chris Pickard <chrispickard9@gmail.com>
2021-06-23 11:29:25 -0400

    updated to use actual packages
    instead of pulling packages directly from github, they are usually
    packaged nicely by maintainers. Prefer them instead. Buffalo isn't
    packaged, so it still uses the old form

--- nix/zsh.nix
+++ nix/zsh.nix
@@ -19,7 +19,7 @@
 
       reload = "exec zsh";
       switch =
-        "rm ~/.config/mimeapps.list || true && home-manager switch; exec zsh";
+        "rm ~/.config/mimeapps.list || true && home-manager switch && exec zsh";
 
       start = "./start.py";
       t = "./.test.sh";
@@ -33,13 +33,8 @@
     plugins = [
       {
         name = "pure";
-        file = "pure.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "sindresorhus";
-          repo = "pure";
-          rev = "3589b7f3f8a687b2a3b7f91100bae540fcd4056f";
-          sha256 = "03nr88349frfkq3rcgm2x5m3r23sgq52dqcpvdkrfi40b610rl17";
-        };
+        src = "${pkgs.pure-prompt}/share/zsh/site-functions/";
+        file = "prompt_pure_setup";
       }
       {
         name = "buffalo.zsh";
@@ -52,46 +47,22 @@
         };
       }
       {
-        name = "_docker";
-        src = pkgs.fetchurl {
-          url =
-            "https://raw.githubusercontent.com/docker/cli/v19.03.8/contrib/completion/zsh/_docker";
-          sha256 = "09b7376a06svy533fla718s6knqssagsb0k74sbfgicgknwgiv6d";
-        };
+        name = "docker";
+        src = "${pkgs.docker}/share/zsh/site-functions/";
+        file = "_docker";
       }
       {
-        name = "_terraform";
-        src = pkgs.fetchurl {
-          url =
-            "https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/terraform/_terraform";
-          sha256 = "0gnyp4r0kcax2ldhzq8anyfaw1y7rm7nf71pylhyq7nkf8fix7c8";
-        };
+        name = "terraform";
+        src = "${pkgs.oh-my-zsh}/share/oh-my-zsh/plugins/terraform";
       }
       {
-        name = "_systemctl";
-        src = pkgs.fetchurl {
-          url =
-            "https://raw.githubusercontent.com/systemd/systemd/v245/shell-completion/zsh/_systemctl.in";
-          sha256 = "0ng65sbgis2v9is34x22bv0wjr5bkhd0v2xi08l6q7ggz5db32yq";
-        };
-      }
-      {
-        name = "_journalctl";
-        src = pkgs.fetchurl {
-          url =
-            "https://raw.githubusercontent.com/systemd/systemd/v245/shell-completion/zsh/_journalctl";
-          sha256 = "1nk0j9b38swyjmfp5amlfqnmm9hj3cmifp0dsnsd8933cf1yvnyg";
-        };
+        name = "systemctl";
+        src = "${pkgs.libudev}/share/zsh/site-functions/";
       }
       {
         name = "zsh-syntax-highlighting";
         file = "zsh-syntax-highlighting.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "zsh-users";
-          repo = "zsh-syntax-highlighting";
-          rev = "f1b9fbbaf0123fb7b14b4a11bc18c86fd4b2305b";
-          sha256 = "0vdcj07b568piil7ix7kd3xdxv9g3r0c0gl1rfrn1lbcspw31a6b";
-        };
+        src = "${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting";
       }
     ];
     # enableCompletion = true;
