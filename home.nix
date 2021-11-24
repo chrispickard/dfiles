e2df9907243ce98892dd88c5833d31c1e1328df3
Chris Pickard <chrispickard9@gmail.com>
2021-11-24 14:54:17 -0500

    updating flake

--- home.nix
+++ home.nix
@@ -56,9 +56,9 @@ in {
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "21.05";
-  home.homeDirectory = "/home/chrispickard";
-  home.username = "chrispickard";
+  # home.stateVersion = "21.05";
+  # home.homeDirectory = "/home/chrispickard";
+  # home.username = "chrispickard";
   home.packages = with pkgs; [
     go
     # goimports
