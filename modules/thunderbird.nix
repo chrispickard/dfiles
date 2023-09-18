66ce116b313c6ae73b7b2e9faf4f9857d3337804
Chris Pickard <chrispickard9@gmail.com>
2023-09-18 13:48:57 -0400

    fixed

--- modules/thunderbird.nix
+++ modules/thunderbird.nix
@@ -1,11 +1,13 @@
 { config, lib, pkgs, ... }:
 
+let thunderbird = pkgs.thunderbird;
+in
 {
   home.packages = [ thunderbird ];
   xsession.windowManager.i3.config.keybindings =
     let leader = "Mod1 + Shift";
     in
     {
-      "${leader}+t" = "exec btf -m thunderbird thunderbird";
+      "${leader}+t" = "exec btf -m thunderbird ${thunderbird}";
     };
 }
